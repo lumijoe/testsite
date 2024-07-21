5a9648f9d7243cd706fd52f7e516ceeabbfa5724
lumijoe <spacedesign63@gmail.com>
2024-07-21 15:44:29 +0900

    first commit

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+function custom_theme_setup()
+{
+    // head内にフィードリンクを出力
+    add_theme_support('automatic-feed-links');
+
+    // 表示ページに合わせたタイトルタグを出力
+    add_theme_support('title-tag');
+
+    // ブロックエディタ用の標準スタイルを有効化
+    add_theme_support('wp-block-styles');
+
+    // 埋め込みコンテンツのレスポンシブを有効化
+    add_theme_support('responsive-embeds');
+}
+add_action('after_setup_theme', 'custom_theme_setup');
+
+function testsite_scripts()
+{
+    wp_enqueue_style(
+        'base-style', // ハンドル名
+        get_theme_file_uri('css/styles.css'), // ファイルのパスを取得
+        array(), // 依存関係
+        '1.0', // バージョン指定
+        'all' // メディアタイプ
+    );
+}
+add_action('wp_enqueue_scripts', 'testsite_scripts');
